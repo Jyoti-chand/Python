347ef13b88fc57568b551082615cfe1a231655c6
Jyoti-chand <155794392+Jyoti-chand@users.noreply.github.com>
2025-03-09 22:25:48 +0545

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,22 @@
+#Arithematic operator 
+a=input("Enter first number:")
+b=input("Enter second number:")
+print(a+b)
+#print(a%b)
+
+#Relational operators
+print(a==b)
+print(a!=b)
+
+#Assignment operator
+a+=b
+print("a")
+
+#logical operator
+print(not(a>b))
+val1=True
+val2=False
+print("AND operator:",val1 and val2)
+print("OR operator:",val1 or val2)
+
+
