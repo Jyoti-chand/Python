9d67c0759fc4377715d3ad58d6fd8bca2c6f291b
Jyoti-chand <155794392+Jyoti-chand@users.noreply.github.com>
2025-03-16 22:02:13 +0545

    Add files via upload

---
+++ sub.py
@@ -0,0 +1,11 @@
+marks={}
+X=int(input("enter physics:"))
+marks.update({"physics":X})
+
+X=int(input("enter chemistry:"))
+marks.update({"chemistry":X})
+
+X=int(input("enter mathematics:"))
+marks.update({"mathematics":X})
+
+print(marks)
